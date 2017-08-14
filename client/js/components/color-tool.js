487543eb44634f3c49ea784fdca5027b8e44da7f
Eric W. Greene <eric@ericwgreene.com>
2017-08-14 13:24:16 -0500

    added dynamic table code

--- client/js/components/color-tool.js
+++ client/js/components/color-tool.js
@@ -2,18 +2,15 @@ import React from 'react';
 
 export class ColorTool extends React.Component {
   render() {
+
+    const colors = ['blue','pink','green','yellow','red','teal','black'];
+
     return <div>
       <header>
         <h1>Color Tool</h1>
       </header>
       <ul>
-        <li>blue</li>
-        <li>pink</li>
-        <li>green</li>
-        <li>yellow</li>
-        <li>red</li>
-        <li>teal</li>
-        <li>black</li>
+        {colors.map(color => <li>{color}</li>)}
       </ul>
     </div>;
   }
