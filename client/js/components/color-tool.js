0938f28bf35d2b28100b3b6e56d33a621c909e63
Eric W. Greene <eric@ericwgreene.com>
2017-08-14 11:26:35 -0500

    added color list

--- client/js/components/color-tool.js
+++ client/js/components/color-tool.js
@@ -2,8 +2,19 @@ import React from 'react';
 
 export class ColorTool extends React.Component {
   render() {
-    return <header>
-      <h1>Color Tool</h1>
-    </header>;
+    return <div>
+      <header>
+        <h1>Color Tool</h1>
+      </header>
+      <ul>
+        <li>blue</li>
+        <li>pink</li>
+        <li>green</li>
+        <li>yellow</li>
+        <li>red</li>
+        <li>teal</li>
+        <li>black</li>
+      </ul>
+    </div>;
   }
 }
