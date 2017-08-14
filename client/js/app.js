76a67d2bd1252d6c36ae5366256f9ede713c6e72
Eric W. Greene <eric@ericwgreene.com>
2017-08-14 14:10:07 -0500

    added styling demo

--- client/js/app.js
+++ client/js/app.js
@@ -1,3 +1,5 @@
+import '../scss/styles.scss';
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 
--- client/js/components/color-tool.js
+++ client/js/components/color-tool.js
@@ -5,12 +5,16 @@ export class ColorTool extends React.Component {
 
     const colors = ['blue','pink','green','yellow','red','teal','black'];
 
+    const boldListItem = { fontWeight: 'bold' };
+
     return <div>
       <header>
         <h1>Color Tool</h1>
       </header>
       <ul>
-        {colors.map(color => <li>{color}</li>)}
+        {colors.map(color =>
+          <li className="artem" style={boldListItem}>{color}</li>
+        )}
       </ul>
     </div>;
   }
