5b528336228032663cb84178cf8ea4bf30280e62
Eric W. Greene <eric@ericwgreene.com>
2017-08-16 11:34:03 -0500

    remove uppercase from tool header

--- client/js/components/tool-header.js
+++ client/js/components/tool-header.js
@@ -1,20 +1,15 @@
 import React from 'react';
 
-export const ToolHeader = props => {
-  
-  const headerTextUpperCase = props.headerText.toUpperCase();
-
-  return <header>
-    <h1>{headerTextUpperCase}</h1>
-  </header>;
-};
+export const ToolHeader = props => <header>
+  <h1>{props.headerText}</h1>
+</header>;
 
 // export class ToolHeader extends React.Component {
   
-//     render() {
-//       return <header>
-//         <h1>{this.props.headerText}</h1>
-//       </header>;
-//     }
-  
+//   render() {
+//     return <header>
+//       <h1>{this.props.headerText}</h1>
+//     </header>;
 //   }
+
+// }
