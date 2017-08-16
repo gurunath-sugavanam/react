7cbc1f238a323c53b0710a3c02b82bf08c55185c
Eric W. Greene <eric@ericwgreene.com>
2017-08-16 15:19:17 -0500

    added keys

--- client/js/components/color-tool.js
+++ client/js/components/color-tool.js
@@ -3,6 +3,34 @@ import React from 'react';
 import { ToolHeader } from './tool-header';
 import { ColorForm } from './color-form';
 
+class ListItem extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      item: props.item,
+    };
+
+    //console.log('list item constructor: ' + props.item);
+  }
+
+
+  componentWillReceiveProps(nextProps) {
+    this.setState({
+      item: nextProps.item,
+    });
+  }
+
+  render() {
+
+    return <li>props: {this.props.item}, state: {this.state.item}</li>;
+
+  }
+
+}
+
+
 export class ColorTool extends React.Component {
 
   constructor(props) {
@@ -11,6 +39,14 @@ export class ColorTool extends React.Component {
     this.state = {
       colors: [ ...props.colors ],
     };
+  }
+
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({
+        colors: [ ...this.props.colors.slice(0,2), ...this.props.colors.slice(3) ]
+      });
+    }, 4000);
   }
 
   addColor = newColor => {
@@ -24,7 +60,7 @@ export class ColorTool extends React.Component {
     return <div>
       <ToolHeader xheaderText={this.props.headerText} />
       <ul>
-        {this.state.colors.map(color => <li>{color}</li>)}
+        {this.state.colors.map( (color, index) => <ListItem key={index} item={color} />)}
       </ul>
       <ColorForm onSubmitColor={this.addColor} />
     </div>;
